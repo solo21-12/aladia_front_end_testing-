9291be39d53a4cf426f075463d7a56a9fc3b70f0
solo2112 <dawitabrham0021@gmail.com>
2024-10-12 13:10:47 +0300

    (dawit_abraham)__new ci

--- cypress.config.js
+++ cypress.config.js
@@ -15,6 +15,6 @@ export default defineConfig({
       return config;
     },
     experimentalStudio: true,
-    baseUrl: "https://localhost:3000/",
+    baseUrl: "http://localhost:3000/",
   },
 });
