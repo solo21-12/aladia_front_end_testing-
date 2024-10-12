e61677961c943fbe3538fafa182a2ae97e4b76f4
solo2112 <dawitabrham0021@gmail.com>
2024-10-12 10:57:13 +0300

    (dawit_abraham)__update with localhost

--- build.sh
+++ build.sh
@@ -1,12 +1,23 @@
+#!/bin/bash
+
 set -e
 
-echo "Installing dependencies"
+projects=("nuxt-app/")
 
-echo "Cleaning up node_modules and cache..."
-rm -rf node_modules
-rm -rf .pnpm-store
+root_directory=$PWD
 
-pnpm install --ignore-scripts
+for project in "${projects[@]}"; do
 
-echo "Building nuxt-app..."
-pnpm run build
+    echo "Installing dependencies for $project"
+    cd "$root_directory/$project"
+
+    echo "Cleaning up node_modules and cache..."
+    rm -rf node_modules
+    rm -rf .pnpm-store
+
+    pnpm install --ignore-scripts
+
+    echo "Building $project..."
+    pnpm run build
+    cd "$root_directory"
+done
