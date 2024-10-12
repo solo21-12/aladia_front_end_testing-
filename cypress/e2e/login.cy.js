cc297004b93f8618b5473c4ef8abb848cca48a04
solo2112 <dawitabrham0021@gmail.com>
2024-10-12 09:33:22 +0300

    (dawit_abraham)__update with localhost

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,7 +2,7 @@ describe("template spec", () => {
   /* ==== Test Created with Cypress Studio ==== */
   it("login user", function() {
     /* ==== Generated with Cypress Studio ==== */
-    cy.visit("/");
+    cy.visit("http://localhost:3000");
     cy.wait(500);
 
     // Take a snapshot of the initial page load
